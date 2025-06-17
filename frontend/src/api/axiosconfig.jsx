d06f61d482e4e8f6b53b05aab29161652e396918
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-06-17 22:17:34 +0530

    base url change

--- frontend/src/api/axiosconfig.jsx
+++ frontend/src/api/axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL:"https://e-commercebackend-1-xg2z.onrender.com",
+  baseURL:"e-commercebackend.railway.internal",
 });
 
 export default instance
