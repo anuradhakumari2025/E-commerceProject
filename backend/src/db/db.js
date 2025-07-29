e69d9f6fb06ced38a20b6f9fa59c200eb204a3df
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-07-29 19:25:25 +0530

    Removed file

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
-    // const conn = await mongoose.connect(
-      // "mongodb+srv://AniketBh:CuXamaT1GuBuph6l@ankur-warikoo.kjqmv4u.mongodb.net/dummy"
-    // );
-
-    console.log("Mongodb connected");
+      console.log("Mongodb connected");
   } catch (error) {
     console.log(error);
   }
